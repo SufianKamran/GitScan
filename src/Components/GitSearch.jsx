5760797ffc23f686f085c3da9c69f7a8abae1c2b
SUFIAN <sufikami@gmail.com>
2024-08-30 02:18:42 +0500

    add margin

--- src/Components/GitSearch.jsx
+++ src/Components/GitSearch.jsx
@@ -70,7 +70,7 @@ function GitSearch() {
           variant="h4"
           sx={{ fontWeight: "bold" }}
         >
-          GitScanner
+          GitScan
         </Typography>
 
         <Stack
@@ -160,7 +160,7 @@ function GitSearch() {
                   {userData.public_repos}
                 </Typography>
               </Stack>
-              <Stack mx={5} alignItems="center">
+              <Stack mx={1} alignItems="center">
                 <Typography>Followers</Typography>
                 <Typography variant="h6" sx={{ fontWeight: "bold" }}>
                   {userData.followers}
